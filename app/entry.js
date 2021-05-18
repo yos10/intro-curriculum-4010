39ceec089ac010f0d89c93497a96f681180c3037
yos10 <61726023+yos10@users.noreply.github.com>
2021-05-18 15:37:46 +0900

    右に移動させるボタンと連打防止

--- app/entry.js
+++ app/entry.js
@@ -2,8 +2,22 @@
 import $ from 'jquery';
 const block = $('#block');
 const scalingButton = $('#scaling-button');
+const movingButton = $('#moving-button');
 
-scalingButton.click(() => {
+scalingButton.on('click', () => {
+  scalingButton.css('pointer-events', 'none');
   block.animate({ width: '200pt', height: '200pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
+  block.promise().done(() => {
+    scalingButton.css('pointer-events', 'auto');
+  });
+});
+
+movingButton.on('click', () => {
+  movingButton.css('pointer-events', 'none');
+  block.animate({ marginLeft: '500px' }, 500);
+  block.animate({ marginLeft: '20px' }, 1000);
+  block.promise().done(() => {
+    movingButton.css('pointer-events', 'auto');
+  });
 });
